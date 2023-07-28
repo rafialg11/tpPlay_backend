6938d4bf8eb8ffd1e575c1dadfd4b0b10ebf506d
Rafi Alghifari <53641104+rafialg11@users.noreply.github.com>
2023-07-28 11:20:45 +0700

    Update videoModel.js

--- models/videoModel.js
+++ models/videoModel.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const videoSchema = new Schema({
-    thumbnail: {
-        type: String,
-        required: true
-    },
-    videoUrl: {
+    urlImgThumbnail: {
         type: String,
         required: true
     }
