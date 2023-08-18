f64c197beb96bde19322bc24f25a12d0adfea839
Rafi Alghifari <raffialg11@gmail.com>
2023-08-18 22:43:16 +0700

    change product route's params

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
     const product = require('../controllers/productController')
     const router = require('express').Router()
 
-    router.get('/:id', product.findAll)
+    router.get('/:videoId', product.findAll)
     // router.get('/:id', product.findOne)
     app.use('/api/products', router)
 }
